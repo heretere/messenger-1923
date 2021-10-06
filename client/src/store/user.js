f5964d4de9749b99b7c59b90d8e58ad8797d3dbc
Heretere <heirteir@protonmail.com>
2021-10-06 13:37:42 -0400

    Automated prettier reformat

--- client/src/store/user.js
+++ client/src/store/user.js
@@ -6,31 +6,31 @@ const SET_FETCHING_STATUS = "SET_FETCHING_STATUS";
 // ACTION CREATORS
 
 export const getUser = (user) => {
-  return {
-    type: GET_USER,
-    user,
-  };
+    return {
+        type: GET_USER,
+        user,
+    };
 };
 
 export const setFetchingStatus = (isFetching) => ({
-  type: SET_FETCHING_STATUS,
-  isFetching,
+    type: SET_FETCHING_STATUS,
+    isFetching,
 });
 
 // REDUCER
 
-const reducer = (state = { isFetching: true }, action) => {
-  switch (action.type) {
-    case GET_USER:
-      return action.user;
-    case SET_FETCHING_STATUS:
-      return {
-        ...state,
-        isFetching: action.isFetching,
-      };
-    default:
-      return state;
-  }
+const reducer = (state = {isFetching: true}, action) => {
+    switch (action.type) {
+        case GET_USER:
+            return action.user;
+        case SET_FETCHING_STATUS:
+            return {
+                ...state,
+                isFetching: action.isFetching,
+            };
+        default:
+            return state;
+    }
 };
 
 export default reducer;
